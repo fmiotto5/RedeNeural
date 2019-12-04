b0fe6339531094059c75f3667644e4637a4d4977
Felipe Miotto <fmiotto5@ucs.br>
2019-12-04 19:46:26 -0300

    Ajustes finais

--- src/principal/Main.java
+++ src/principal/Main.java
@@ -5,10 +5,5 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) {
         Algoritmo a = new Algoritmo();
-
-        //por que a primeira instância não converge para 1000000000?
-        //printar o erro da camada intermediaria pra ver se tem algum padrao de erro na it 2 para a 3
-        //
-        //depurar
     }
 }
